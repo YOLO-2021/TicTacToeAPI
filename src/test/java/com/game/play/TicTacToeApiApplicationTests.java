5123308aa7d7a8cac7bee459e62ff56cce2b7af6
Sarankumar <narasramuk@gmail.com>
2021-08-29 13:35:44 +0200

    sample tests added

--- src/test/java/com/game/play/TicTacToeApiApplicationTests.java
+++ src/test/java/com/game/play/TicTacToeApiApplicationTests.java
@@ -1,13 +1,48 @@
 package com.game.play;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.game.play.controller.TicTacToeController;
+import com.game.play.model.Game;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class TicTacToeApiApplicationTests {
 
+	@Autowired
+	TicTacToeController controller;
+
 	@Test
-	void contextLoads() {
+	public void testNewGame() {
+		
+		ResponseEntity<Game> s = controller.startGame();
+		assertEquals(HttpStatus.OK, s.getStatusCode());
+		
 	}
+	
+	@Test
+	public void testPlayGame() {
+		
+		ResponseEntity<Game> s = controller.playGame("X");
+		assertEquals(HttpStatus.OK, s.getStatusCode());
+		
+	}
+	
+	@Test
+	public void testGameStatus() {
+		
+		ResponseEntity<Game> s = controller.gameStatus("id");
+		assertEquals(HttpStatus.OK, s.getStatusCode());
+		
+	}
+	
 
 }
